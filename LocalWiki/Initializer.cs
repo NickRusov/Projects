fe52441b97bcd40150929277714a3de9351f6d83
Nikolay Rusov <nikolay.rusov@firstlinesoftware.com>
2015-05-07 20:04:17 +0300

    With ProEntity

--- LocalWiki/Initializer.cs
+++ LocalWiki/Initializer.cs
@@ -10,43 +10,43 @@ namespace LocalWiki
     {
         public static Facade GetIniatializedFacade()
         {
-            UserRepository userRepository = new UserRepository();
-            User firstUser = new User("Nick", "Rusov", 20);
-            User secondUser = new User("Nickola", "Dusov", 24);
-            User thirdUser = new User("Nickola", "Shrusov", 22);
+            var userRepository = new UserRepository();
+            var firstUser = new User("Nick", "Rusov", 20);
+            var secondUser = new User("Nickola", "Dusov", 24);
+            var thirdUser = new User("Nickola", "Shrusov", 22);
             userRepository.AddUser(firstUser);
             userRepository.AddUser(secondUser);
             userRepository.AddUser(thirdUser);
 
-            AuthorRepository authorRepository = new AuthorRepository();
-            Author firstAuthor= new Author("Nickolay", "Pusov", 28, "johndoe@example.com");
-            Author secondAuthor = new Author(firstUser, "jamesdon@example.com");
-            Author thirdAuthor = new Author(secondUser, "jamesdon@example.com");
+            var authorRepository = new AuthorRepository();
+            var firstAuthor = new Author("Nickolay", "Pusov", 28, "johndoe@example.com");
+            var secondAuthor = new Author(firstUser, "jamesdon@example.com");
+            var thirdAuthor = new Author(secondUser, "jamesdon@example.com");
             authorRepository.AddAuthor(new Author(thirdUser, "jamesdon@example.com"));
             authorRepository.AddAuthor(secondAuthor);
             authorRepository.AddAuthor(firstAuthor);
             authorRepository.AddAuthor(thirdAuthor);
 
-            AdminRepository adminRepository = new AdminRepository();
+            var adminRepository = new AdminRepository();
             string[] privelegies = { "Delete articles", "Delete comments" };
-            Admin firstAdmin = new Admin((User)firstAuthor, privelegies);
-            Admin secondAdmin = new Admin((User)thirdAuthor, privelegies);
+            var firstAdmin = new Admin((User)firstAuthor, privelegies);
+            var secondAdmin = new Admin((User)thirdAuthor, privelegies);
             adminRepository.AddAdmin(new Admin(thirdUser, privelegies));
             adminRepository.AddAdmin(firstAdmin);
             adminRepository.AddAdmin(secondAdmin);
 
 
-            Article article = new Article(new Author(secondAdmin,"mail:soap"), "C# classes", "Some text");
+            var article = new Article(new Author(secondAdmin, "mail:soap"), "C# classes", "Some text");
 
-            Comment firstComment= new Comment("Cool!", thirdAuthor);
+            var firstComment = new Comment("Cool!", thirdAuthor);
             article.AddComment(new Comment("Not bad.", secondAdmin));
-            Rating firstRating = new Rating(firstComment, 8);
-            Rating secondRating = new Rating("Really cool!", firstAdmin, 10);
+            var firstRating = new Rating(firstComment, 8);
+            var secondRating = new Rating("Really cool!", firstAdmin, 10);
             article.AddComment(firstComment);
             article.AddRating(firstRating);
             article.AddRating(secondRating);
 
-            ArticleRepository articleRepository = new ArticleRepository();
+            var articleRepository = new ArticleRepository();
             articleRepository.AddArticle(article);
             articleRepository.AddArticle(new Article(new Author("b", "Rusov", 20, "mail address"), "C# interfaces",
                 "Some text about interfaces"));
